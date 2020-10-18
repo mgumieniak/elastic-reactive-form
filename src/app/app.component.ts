fe62131997e7b04d5cacb50a3c11dd2af39fa288
mgumieniak <maciejgumieniak007@gmail.com>
2020-10-18 23:13:55 +0200

    Prepare elastic reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, AfterViewInit, ChangeDetectorRef} from '@angular/core';
+import { Validators } from '@angular/forms';
+
+import { FieldConfig } from './dynamic-form/models/field-config.interface';
+import { DynamicFormComponent } from './dynamic-form/containers/dynamic-form/dynamic-form.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['app.component.scss'],
+  template: `
+    <div class="app">
+      <dynamic-form
+        [config]="config"
+        #form="dynamicForm"
+        (submit)="submit($event)">
+      </dynamic-form>
+      {{ form.valid }}
+      {{ form.value | json }}
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'elastic-reactive-form';
+export class AppComponent implements AfterViewInit {
+  @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
+
+  config: FieldConfig[] = [
+    {
+      type: 'input',
+      label: 'Full name',
+      name: 'name',
+      placeholder: 'Enter your name',
+      validation: [Validators.required, Validators.minLength(4)]
+    },
+    {
+      type: 'select',
+      label: 'Favourite Food',
+      name: 'food',
+      options: ['Pizza', 'Hot Dogs', 'Knakworstje', 'Coffee'],
+      placeholder: 'Select an option',
+      validation: [Validators.required]
+    },
+    {
+      label: 'Submit',
+      name: 'submit',
+      type: 'button'
+    }
+  ];
+
+
+  constructor(private cdr: ChangeDetectorRef) {
+  }
+
+  ngAfterViewInit() {
+    let previousValid = this.form.valid;
+    this.form.changes.subscribe(() => {
+      if (this.form.valid !== previousValid) {
+        previousValid = this.form.valid;
+        this.form.setDisabled('submit', !previousValid);
+      }
+    });
+
+    this.form.setDisabled('submit', true);
+    this.form.setValue('name', 'Todd Motto');
+    this.cdr.detectChanges();
+  }
+
+  submit(value: {[name: string]: any}) {
+    console.log(value);
+  }
 }
---
+++ src/app/dynamic-form/components/dynamic-field/dynamic-field.directive.ts
@@ -0,0 +1,58 @@
+import {
+  ChangeDetectorRef,
+  ComponentFactoryResolver,
+  ComponentRef,
+  Directive,
+  Input,
+  OnChanges,
+  OnInit,
+  Type,
+  ViewContainerRef
+} from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { FormButtonComponent } from '../form-button/form-button.component';
+import { FormInputComponent } from '../form-input/form-input.component';
+import { FormSelectComponent } from '../form-select/form-select.component';
+
+import { Field } from '../../models/field.interface';
+import { FieldConfig } from '../../models/field-config.interface';
+
+const components: {[type: string]: Type<Field>} = {
+  button: FormButtonComponent,
+  input: FormInputComponent,
+  select: FormSelectComponent
+};
+
+@Directive({
+  selector: '[dynamicField]'
+})
+export class DynamicFieldDirective implements Field,  OnInit {
+  @Input()
+  config: FieldConfig;
+
+  @Input()
+  group: FormGroup;
+
+  component: ComponentRef<Field>;
+
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private container: ViewContainerRef,
+  ) {}
+
+
+  ngOnInit() {
+    if (!components[this.config.type]) {
+      const supportedTypes = Object.keys(components).join(', ');
+      throw new Error(
+        `Trying to use an unsupported type (${this.config.type}).
+        Supported types: ${supportedTypes}`
+      );
+    }
+    const componentFactory = this.resolver.resolveComponentFactory<Field>(components[this.config.type]);
+    this.component = this.container.createComponent(componentFactory);
+    this.component.instance.config = this.config;
+    this.component.instance.group = this.group;
+  }
+}
